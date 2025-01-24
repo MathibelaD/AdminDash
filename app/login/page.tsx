3e468b916f74e95defa7b25668a80cb10797f26a
siphonhata <siphonhata@gmail.com>
2025-01-24 23:15:28 +0200

    testing branches

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../ui/Login/loginForm";
+
+export default function Page(){
+    return(
+        <div className="w-full">
+            <LoginForm />
+        </div>
+    );
+}
