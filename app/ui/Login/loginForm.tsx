82ecdc80f12e1b93af38cb173031734e299b31d3
siphonhata <siphonhata@gmail.com>
2025-01-25 00:15:51 +0200

    Dashboard Menu Content

--- app/ui/Login/loginForm.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginForm() {
-    
-    return(
-        <div className="mt-52">
-            Testing Branches
-        </div>
-    );
-}
---
+++ components/Dashboard/Menu/table.tsx
@@ -0,0 +1,5 @@
+export default function MenuTable() {
+    return (
+        <h1>Tables</h1>
+    )
+}
