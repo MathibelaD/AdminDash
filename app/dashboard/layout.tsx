8f54c443c9f8bfddd0e267d2e22bb950e9ebd480
MathibelaD <dineomathibela161@gmail.com>
2026-05-28 18:57:08 +0200

    fix errors

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,17 +1,8 @@
 import HeaderComponent from '@/components/Dashboard/HeaderComponent';
 import Sidebar from '@/components/Dashboard/nav-links';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/lib/auth';
-import { redirect } from 'next/navigation';
 import React from 'react';
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    redirect('/');
-  }
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="fixed top-0 left-0 w-full z-10">
