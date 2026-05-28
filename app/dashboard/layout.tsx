be21389f7559369aab018aad647e5cfa56e65908
MathibelaD <dineomathibela161@gmail.com>
2026-05-28 18:16:49 +0200

    fix build errors

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,26 @@
-
 import HeaderComponent from '@/components/Dashboard/HeaderComponent';
 import Sidebar from '@/components/Dashboard/nav-links';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/lib/auth';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+  if (!session) {
+    redirect('/');
+  }
+
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* Header - Fixed at the top */}
-      <div className="fixed top- left-0 w-full z-10">
+      <div className="fixed top-0 left-0 w-full z-10">
         <HeaderComponent />
       </div>
 
-      {/* Main container */}
-      <div className="flex pt-16"> {/* pt-16 to account for fixed header height */}
-        {/* Sidebar */}
+      <div className="flex pt-16">
         <Sidebar />
 
-        {/* Main content */}
         <main className="flex-1 ml-64 p-6 min-h-screen bg-gray-50">
           {children}
         </main>
